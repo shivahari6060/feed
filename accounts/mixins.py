082fb3710f3dd90465800790660fc31d4bf8be81
shivahari6060 <devkota.shivahari@gmail.com>
2025-08-04 07:48:49 +0545

    'superuser'

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -7,7 +7,7 @@ class RoleRequiredMixin:
     required_roles = []
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated or request.user.user_type not in self.required_roles:
+        if not request.user.is_authenticated or request.user.is_superuser or request.user.user_type not in self.required_roles:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
 
@@ -23,21 +23,3 @@ class ProducerRequiredMixin(RoleRequiredMixin):
 
 
 
-from functools import wraps
-from .constants import Role
-
-def admin_required(view_func):
-    @wraps(view_func)
-    def _wrapped_view(request, *args, **kwargs):
-        if not request.user.is_authenticated or request.user.user_type != Role.ADMIN:
-            raise PermissionDenied()
-        return view_func(request, *args, **kwargs)
-    return _wrapped_view
-
-def inventory_required(view_func):
-    @wraps(view_func)
-    def _wrapped_view(request, *args, **kwargs):
-        if not request.user.is_authenticated or request.user.user_type not in [Role.ADMIN, Role.INVENTORY]:
-            raise PermissionDenied()
-        return view_func(request, *args, **kwargs)
-    return _wrapped_view
